9f6b4989e34880f0c1c4e0c75a18346647aa4931
Eric Halpern <ehalpern@attic.com>
2017-09-20 14:27:18 -0700

    More config tweaking

--- presto-noms/deployment/hooks/after_install.sh
+++ presto-noms/deployment/hooks/after_install.sh
@@ -7,16 +7,21 @@ ROLE=$(role_from_asg $ASG)
 DISCOVERY_URI=http://master.presto.attic.io:8080
 
 config=/etc/presto/config.properties
+node=/etc/presto/node.properties
 
-sed -i "s#\(^discovery-uri=\).*#\1=${DISCOVERY_URI}#g" ${config}
+sed -i "s#\(^discovery.uri=\).*#\1${DISCOVERY_URI}#g" ${config}
+sed -i 's/\(^node.environment==\).*/\1production/g' ${node}
+
 
 if [ "$ROLE" == "master" ]; then
-    sed -i 's/\(^coordinator=\).*/\1=true/g' ${config}
-    sed -i 's/\(^discovery-server.enabled\)=.*/\1=true/g' ${config}
-    sed -i 's/\(^node-scheduler.include-coordinator\)=.*/\1=false/g' ${config}
-    aws-ec2-assign-elastic-ip --valid-ips 50.112.87.220
+    sed -i 's/\(^coordinator=\).*/\1true/g' ${config}
+    sed -i 's/\(^discovery-server.enabled=\).*/\1true/g' ${config}
+    sed -i 's/\(^node-scheduler.include-coordinator=\).*/\1false/g' ${config}
+
+    masterip=$(host master.presto.attic.io | awk '{ print $4 }')
+    aws-ec2-assign-elastic-ip --valid-ips ${masterip}
 else
-    sed -i 's/\(^coordinator=\).*/\1=false/g' ${config}
-    sed -i 's/\(^discovery-server.enabled\)=.*/\1=false/g' ${config}
-    sed -i 's/\(^node-scheduler.include-coordinator\)=.*/\1=false/g' ${config}
+    sed -i 's/\(^coordinator=\).*/\1false/g' ${config}
+    sed -i 's/\(^discovery-server.enabled=\).*/\1false/g' ${config}
+    sed -i 's/\(^node-scheduler.include-coordinator=\).*/\1false/g' ${config}
 fi
