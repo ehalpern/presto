e942fe3a8965f939ef10c94eb76a8b20c1bc82fe
Eric Halpern <ehalpern@attic.com>
2017-09-20 00:00:52 -0700

    Auto assign elastic ip to master

--- presto-noms/deployment/hooks/after_install.sh
+++ presto-noms/deployment/hooks/after_install.sh
@@ -4,7 +4,7 @@
 
 ASG=$(get_autoscaling_group)
 ROLE=$(role_from_asg $ASG)
-DISCOVERY_URI=http://localhost:8080
+DISCOVERY_URI=http://master.presto.attic.io:8080
 
 config=/etc/presto/config.properties
 
@@ -14,6 +14,7 @@ if [ "$ROLE" == "master" ]; then
     sed -i 's/\(^coordinator=\).*/\1=true/g' ${config}
     sed -i 's/\(^discovery-server.enabled\)=.*/\1=true/g' ${config}
     sed -i 's/\(^node-scheduler.include-coordinator\)=.*/\1=false/g' ${config}
+    aws-ec2-assign-elastic-ip --valid-ips 50.112.87.220
 else
     sed -i 's/\(^coordinator=\).*/\1=false/g' ${config}
     sed -i 's/\(^discovery-server.enabled\)=.*/\1=false/g' ${config}
