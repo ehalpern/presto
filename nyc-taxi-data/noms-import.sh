347b6fe14e0dd7cff4ff53f9d4a7ad4c9d8a9417
Dan Willhite <willhite@users.noreply.github.com>
2017-10-03 15:11:30 -0700

    Minor fixes to noms-import.sh

--- nyc-taxi-data/noms-import.sh
+++ nyc-taxi-data/noms-import.sh
@@ -9,7 +9,7 @@ SIZE="100"
 LINE_CHUNK_SIZE=2500000
 LINE_CHUNK_PER_FILE=8
 TEMP_CSV_DIR=/tmp/csv-import
-USAGE="usage: noms-import [--size 100|1000] [--dir /path/to/csv/files] [--append true|false] dsspec"
+USAGE="usage: noms-import [--size 100|1000] [--skip n] [--dir /path/to/csv/files] [--append true|false] dsspec"
 
 lineChunksToSkip=0
 filesToSkip=0
@@ -52,104 +52,13 @@ cd $DIR
 datafiles=`ls trips_*.csv.gz`
 DSSPEC="${ARGS[0]}"
 
-ctr1=0
+cntr1=0
 firstTime=true
 for fn in $datafiles
 do
   if [[ $cntr1 -ge $filesToSkip ]]
   then
-    if [[ $cntr1 -lt 5 ]]
-    then
-      echo "fn: $fn, bn: $bn"
-      rm -rf ${TEMP_CSV_DIR}
-      mkdir ${TEMP_CSV_DIR}
-      gunzip -c $fn | split - "${TEMP_CSV_DIR}/csv-import-" --lines ${LINE_CHUNK_SIZE} --additional-suffix=.csv
-
-Permission denied (publickey).
-#!/usr/bin/env bash
-
-# Args with default values
-APPEND="false"
-ARGS=()
-DIR="$HOME/export/trips"
-DSSPEC=""
-SIZE="100"
-LINE_CHUNK_SIZE=2500000
-LINE_CHUNK_PER_FILE=8
-TEMP_CSV_DIR=/tmp/csv-import
-USAGE="usage: noms-import [--size 100|1000] [--dir /path/to/csv/files] [--append true|false] dsspec"
-
-lineChunksToSkip=0
-filesToSkip=0
-chunksToSkip=0
-
-while [[ $# -gt 0 ]]
-do
-  key="$1"
-
-  case $key in
-...skipping...
-#!/usr/bin/env bash
-
-# Args with default values
-APPEND="false"
-ARGS=()
-DIR="$HOME/export/trips"
-DSSPEC=""
-SIZE="100"
-LINE_CHUNK_SIZE=2500000
-LINE_CHUNK_PER_FILE=8
-TEMP_CSV_DIR=/tmp/csv-import
-USAGE="usage: noms-import [--size 100|1000] [--dir /path/to/csv/files] [--append true|false] dsspec"
-
-lineChunksToSkip=0
-filesToSkip=0
-chunksToSkip=0
-
-while [[ $# -gt 0 ]]
-do
-  key="$1"
-
-  case $key in
-      -s|--size)
-      SIZE="$2"
-      shift # past argument
-      ;;
-      -k|--skip)
-      SKIP="$2"
-      shift # past argument
-      x=$(( $SKIP % $LINE_CHUNK_SIZE ))
-      lineChunksToSkip=$(( $SKIP / $LINE_CHUNK_SIZE ))
-      filesToSkip=$(( $lineChunksToSkip / $LINE_CHUNK_PER_FILE ))
-      chunksToSkip=$(( $lineChunksToSkip % $LINE_CHUNK_PER_FILE ))
-      echo "Skipping  $lineChunksToSkip file chunks (total), $filesToSkip files, $chunksToSkip chunks"
-      ;;
-      -d|--dir)
-      DIR="$2"
-      shift # past argument
-      ;;
-      -a|--append)
-      APPEND="$2"
-      shift # past argument
-      ;;
-      *)
-      ARGS+=($1) # unnamed args
-      ;;
-  esac
-  shift # past argument or value
-done
-
-cd $DIR
-datafiles=`ls trips_*.csv.gz`
-DSSPEC="${ARGS[0]}"
-
-ctr1=0
-firstTime=true
-for fn in $datafiles
-do
-  if [[ $cntr1 -ge $filesToSkip ]]
-  then
-    if [[ $cntr1 -lt 5 ]]
+    if [[ $cntr1 -lt 5 ]] || [[ $SIZE -gt 100 ]]
     then
       echo "fn: $fn, bn: $bn"
       rm -rf ${TEMP_CSV_DIR}
@@ -159,18 +68,18 @@ do
       cntr2=0
       for file in `ls ${TEMP_CSV_DIR}`
       do
-        if [[ $cntr2 -ge $chunksToSkip ]]
-        then
-          csv-import --header=`cat trips.header.csv` --column-types=`cat trips.types.csv` --invert --append=${APPEND} ${TEMP_CSV_DIR}/${file} "${DSSPEC}"
+	if [[ $cntr2 -ge $chunksToSkip ]]
+	then
+	  csv-import --header=`cat trips.header.csv` --column-types=`cat trips.types.csv` --invert --append=${APPEND} ${TEMP_CSV_DIR}/${file} "${DSSPEC}"
           exitcode=$?
-          if [[ $exitcode -ne 0 ]]
-          then
-            echo "csv-import exited with non-zero status: ${exitcode}"
-            exit 1
-          fi
-          APPEND="true"
-        fi
-        (( cntr2++ ))
+	  if [[ $exitcode -ne 0 ]]
+	  then
+	    echo "csv-import exited with non-zero status: ${exitcode}"
+	    exit 1
+	  fi
+	  APPEND="true"
+	fi
+	(( cntr2++ ))
       done
       chunksToSkip=0
     fi
